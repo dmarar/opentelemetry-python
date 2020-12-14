a64850f804d9e4309e360d10e8b87fa9fcf25bfa
github-action-benchmark <github@users.noreply.github.com>
2020-12-14 15:55:17 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - (pytest) benchmark result for 2e734be842c50e71bd05b73336ef5c39f59470be

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1607958657596,
+  "lastUpdate": 1607961317213,
   "repoUrl": "https://github.com/dmarar/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.9 -": [
@@ -78,6 +78,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00010284949566608537",
             "extra": "mean: 60.65534162205497 usec\nrounds: 8606"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "55284676+dmarar@users.noreply.github.com",
+            "name": "Dilip M",
+            "username": "dmarar"
+          },
+          "committer": {
+            "email": "55284676+dmarar@users.noreply.github.com",
+            "name": "Dilip M",
+            "username": "dmarar"
+          },
+          "distinct": true,
+          "id": "2e734be842c50e71bd05b73336ef5c39f59470be",
+          "message": "small change to logging message",
+          "timestamp": "2020-12-14T21:23:11+05:30",
+          "tree_id": "a695011f43b72bafaa136ae0a081b7ef02bd522b",
+          "url": "https://github.com/dmarar/opentelemetry-python/commit/2e734be842c50e71bd05b73336ef5c39f59470be"
+        },
+        "date": 1607961316231,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 32050.288498554473,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000011582235406005887",
+            "extra": "mean: 31.20096719394903 usec\nrounds: 5121"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 23937.551404135418,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000014472661468698767",
+            "extra": "mean: 41.77536720933125 usec\nrounds: 7862"
           }
         ]
       }
